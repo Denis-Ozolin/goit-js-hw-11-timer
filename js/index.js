9481f0b7fe99048cf8b7d0798d6ae9d77a98957d
Denis-Ozolin <ozolindenx@gmail.com>
2021-03-07 23:00:41 +0200

    error

--- js/index.js
+++ js/index.js
@@ -1,33 +1,51 @@
-import CountdownTimer from "./timer.js";
-
-const refs = {
-  days: document.querySelector('span[data-value="days"]'),
-  hours: document.querySelector('span[data-value="hours"]'),
-  mins: document.querySelector('span[data-value="mins"]'),
-  secs: document.querySelector('span[data-value="secs"]'),
-}
-
-// function updateTimer({days, hours, mins, secs}){
-//   refs.days.textContent = days;
-//   refs.hours.textContent = hours;
-//   refs.mins.textContent = mins;
-//   refs.secs.textContent = secs;
-// }
-
 const timerSettings = {
   selector: '#timer-1',
-  targetDate: new Date('Mar 8, 2021'),
-  onTick: function updateTimer({days, hours, mins, secs}){
-    refs.days.textContent = days;
-    refs.hours.textContent = hours;
-    refs.mins.textContent = mins;
-    refs.secs.textContent = secs;
+  targetDate: new Date('Mar 10, 2021'),
+}
+
+class CountdownTimer{
+  consructor ({selector, targetDate}){
+    this.selector = selector;
+    this.targetDate = targetDate.getTime();
+    this.startTimer = this.startTimer.bind(this);
+    this.refs = {
+      days: document.querySelector('[data-value="days"]'),
+      hours: document.querySelector('[data-value="hours"]'),
+      mins: document.querySelector('[data-value="mins"]'),
+      secs: document.querySelector('[data-value="secs"]'),
+    }
   }
-};
+
+  pad(value){
+    return String(value).padStart(2, '0');
+  }
+
+  getTimeComponent(time){
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+  
+    return {days, hours, mins, secs};
+  }
+
+  updateTimer({days, hours, mins, secs}){
+    this.refs.days.textContent = days;
+    this.refs.hours.textContent = hours;
+    this.refs.mins.textContent = mins;
+    this.refs.secs.textContent = secs;
+  }
+
+  startTimer(){
+    setInterval(() => {
+      const currentDate = Date.now();
+      const time = this.targetDate - currentDate;    
+      const componentedTime = this.getTimeComponent(time);
+      this.updateTimer(componentedTime);
+    }, 1000);
+  }
+}
 
 const countdownTimer = new CountdownTimer(timerSettings);
 
-countdownTimer.start();
-
-
-
+// countdownTimer.startTimer();
